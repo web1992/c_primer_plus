5ecec3898809bcbae3041f01f25220819d15419a
web1992 <554933654@qq.com>
2016-04-01 23:19:39 +0800

    add toobig

--- c_data_type/toobig.c
+++ c_data_type/toobig.c
@@ -8,6 +8,7 @@ int main(void)
 	unsigned int j=4294967295;	
 	printf("num=%d %d %d \n",num,num+1,num+2);
 	printf("j=%u %u %u \n",j,j+1,j+2);
-	
+	// int  有符号整数，超过最大范围值，从负数开始
+	// unsigned int 无符号整数，超过最大范围值，从0开始	
 	return 0;
 }
