6507c41f085b7f807e207042d65ad54a76359c4d
web1992 <554933654@qq.com>
2016-04-16 23:56:24 +0800

    add

---
+++ c_data_type/const.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main(void)
+{
+	// limits 头文件中包含了整数常量等信息，
+	// 32位系统与64位系统存在差别
+	printf("Maximun int value on this system = %d \n",INT_MAX);
+
+}
