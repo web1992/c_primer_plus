4e392ceb369bbfc200bf2439e1ebe24203c647c2
web1992 <554933654@qq.com>
2016-04-18 09:07:48 +0800

    add c swap function demo

--- c_function/swap.c
+++ c_function/swap.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 void swap(int *,int *);
 int main(void)
-{
+{	
+	// & 地址运算符：获取变量的内存地址
+	// * 间接运算符（或者取值运算符）：通过指针（内存地址）获取变量的值
 	int num0=10;
 	int num1=5;
 	printf("old num0=%d, num1=%d \n",num0,num1);
