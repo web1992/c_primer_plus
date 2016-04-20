9c249f7ee9bbb130e4e510c3d69ce8abec767597
web1992 <554933654@qq.com>
2016-04-20 11:10:16 +0800

    add c++

---
+++ c_++/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// 编译次程序，请使用g++命令
+// 可自行Google g++与gcc区别 
+int main() {
+	    	cout << "Hello, World!" << endl;
+	        cout << "123" << endl;
+		// cout 标准输出
+		// endl == \n 换行符
+		return 0;
+}
