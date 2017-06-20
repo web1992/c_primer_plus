922735da3453ff8fcafffeaf9b4278f860d200c4
web1992 <554933654@qq.com>
2017-06-20 19:09:56 +0800

    add [c scanf]

---
+++ c_scanf/scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//
+int main(void)
+{
+	// code ...
+	int a;
+	int b;
+        while(scanf("%d %d",&a,&b)==2){
+		
+		printf("a=%d \n",a);	
+		printf("b=%d \n",b);	
+	}	
+	return 0;
+}
