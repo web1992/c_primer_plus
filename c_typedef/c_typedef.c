b2ceb5ef3e4cbec9605dc9208795548031983dd9
web1992 <554933654@qq.com>
2017-07-01 14:19:13 +0800

    add [c typedef]

---
+++ c_typedef/c_typedef.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// typedef 在c中的用例
+// https://www.tutorialspoint.com/cprogramming/c_typedef.htm 
+// typedef vs #define
+// #define 在预处理是解析 pre-processor
+// typedef 在则是有编译器 解析的 compiler 
+
+
+typedef struct Book{
+
+	double price;
+	char *name;
+	int id;
+
+} Book;
+
+int main(void)
+{
+	// code ...
+	Book book;
+	book.id = 1;
+	book.name = "C plus plus";
+	book.price = 21.50;	
+
+	printf("Book id = %d name = %s price = %f \n",book.id,book.name,book.price);
+	return 0;
+}
